d9f6fd41be27acdcbe012288862adb2a4f48c26f
yichen <yichenzhu1337@gmail.com>
2014-11-10 21:22:13 -0500

    bower updated with angular

--- app/views/index.php
+++ app/views/index.php
@@ -1,12 +1,49 @@
 <!doctype html>
-<html lang="en" ng-app="app">
+<html>
 
     <head>
-
+        <title> My Esports World </title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="app/images/favicon.ico">
+        <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="app/css/styles.css" rel="stylesheet" type="text/css"/>
+        <link href="app/css/animations.css" rel="stylesheet" type="text/css"/>
     </head>
 
-    <body>
+    <body ng-app="MewApp">
+
+        <div class="container">
+
+            <header>
+                <nav class="navbar navbar-default">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#/customers">Customers</a></li>
+                        <li><a href="#/orders">Orders</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <div ng-view class="slide-animation"></div>
+
+        </div>
+
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/angular/angular.min.js"></script>
+        <script src="bower_components/angular-route/angular-route.min.js"></script>
+        <script src="bower_components/angular-animate/angular-animate.min.js"></script>
+
+        <script src="app/app.js"></script>
+        <script src="app/controllers/customersController.js"></script>
+        <script src="app/controllers/ordersController.js"></script>
+        <script src="app/controllers/allOrdersController.js"></script>
+        <script src="app/services/customersFactory.js"></script>
+        <script src="app/services/customersService.js"></script>
+        <script src="app/services/values.js"></script>
 
     </body>
-
 </html>
---
+++ public/app/services/customersFactory.js
@@ -0,0 +1,29 @@
+(function() {
+    var customersFactory = function($http) {
+
+        var factory = {};
+
+        factory.getCustomers = function() {
+            return $http.get('/api/v1/profiles');
+        };
+
+        factory.getCustomer = function(customerId) {
+            return $http.get('/customers/' + customerId);
+        };
+
+        factory.getOrders = function() {
+            return $http.get('/orders');
+        };
+
+        factory.deleteCustomer = function(customerId) {
+            return $http.delete('/customers/' + customerId);
+        }
+
+        return factory;
+    };
+
+    customersFactory.$inject = ['$http'];
+
+    angular.module('MewApp').factory('customersFactory', customersFactory);
+
+}());
