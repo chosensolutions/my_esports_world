53be1fd5e0fcfef1f8c72e0d8c1a6b2164d63671
yichen <yichenzhu1337@gmail.com>
2016-01-16 07:13:55 -0500

    auth

---
+++ public/app/js/modules/authentication/login/login.controller.js
@@ -0,0 +1,53 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module('app.auth')
+        .controller('AuthenticationRegisterController', AuthenticationRegisterController);
+
+    function AuthenticationRegisterController($state, AuthenticationService) {
+
+        var vm = this;
+
+        vm.input = {
+            type: 'normal',
+            email: 'test@test.com',
+            password: 'password'
+        };
+
+        vm.register = function()
+        {
+            AuthenticationService
+                .register(vm.input)
+                .then(function successCallback(response)
+                {
+                    console.log('---------------------------------');
+                    console.log('---------- Success --------------');
+                    console.log('---------------------------------');
+                    console.log('Code:', response.data.code);
+                    console.log('Message:', response.data.message);
+                    console.log('Data:', response.data.data);
+                    console.log('Status Code: ' + response.status);
+                    console.log('Status Text: ' + response.statusText);
+                    $state.go('home');
+                    toastr.success('You will now be redirected to the home page.', 'Registration Success!');
+                }, function errorCallback(response)
+                {
+                    console.log('---------------------------------');
+                    console.log('---------- Error ----------------');
+                    console.log('---------------------------------');
+                    console.log('Code:', response.data.code);
+                    console.log('Message:', response.data.message);
+                    console.log('Data:', response.data.data);
+                    console.log('Status Code: ' + response.status);
+                    console.log('Status Text: ' + response.statusText);
+
+                    _(response.data.data).forEach(function(value, key) {
+                        toastr.error(value, 'Registration Failed.');
+                    });
+                });
+        };
+    }
+
+})();
