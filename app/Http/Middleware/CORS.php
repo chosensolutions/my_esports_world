8e399de2f49324223ce1fc021bf3fcf55f521705
yichen <yichenzhu1337@gmail.com>
2016-01-16 20:34:02 -0500

    generic message test

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -1,10 +1,11 @@
-<?php namespace App\Http\Middleware;
+<?php
+
+namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Contracts\Routing\Middleware;
 use Illuminate\Http\Response;
 
-class CORS implements Middleware {
+class CORS {
 
     /**
      * Handle an incoming request.
