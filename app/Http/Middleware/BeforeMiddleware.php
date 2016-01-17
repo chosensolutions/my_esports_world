08dcabeb1527d63b6bbc8545a65c168f94b955dc
yichen <yichenzhu1337@gmail.com>
2016-01-17 07:35:52 -0500

    generic message test

--- app/Http/Middleware/BeforeMiddleware.php
+++ app/Http/Middleware/BeforeMiddleware.php
@@ -18,7 +18,7 @@ class BeforeMiddleware
         return $next($request)
             ->header('Access-Control-Allow-Origin', 'https://www.facebook.com/')
             //->header('Access-Control-Allow-Origin', '*')
-            ->header('Allow', 'GET, POST, OPTIONS')
+            //->header('Allow', 'GET, POST, OPTIONS')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
     }
