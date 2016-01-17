6de9f18176cf7e8f7fa291a43ab96e2b7133524f
yichen <yichenzhu1337@gmail.com>
2016-01-16 20:28:38 -0500

    generic message test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,10 @@
 <?php
 
 session_start();
-
+header('Access-Control-Allow-Origin', 'some url');
+header('Allow', 'GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
+header('Access-Control-Allow-Credentials', 'true');
 /**
  * SPA
  */
