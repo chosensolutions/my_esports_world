7b79969e51dabaf8e40bcc28692879389d5fd4a6
yichen <yichenzhu1337@gmail.com>
2016-01-17 16:18:39 -0500

    TestCase.php

---
+++ tests/ProfileTests/ProfileTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Artisan;
+
+class ProfileTest extends TestCase
+{
+    use DatabaseMigrations, WithoutMiddleware;
+
+    /**
+     * Setup:
+     * 1. Migrate the database
+     * 2. Set mail to not send
+     * 3. Set the Test Dummy Factory
+     */
+    public function setUp()
+    {
+        Parent::setUp();
+        //Artisan::call('migrate');
+        //$this->artisan('db:seed');
+    }
+
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        \App\User::create(['email' => 'a@a.com1']);
+        $this->seeInDatabase('users', [
+            'email' => 'a@a.com1'
+        ]);
+    }
+
+}
