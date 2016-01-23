5b48fb8df7739d861b3049e96a9455a37bdea097
yichen <yichenzhu1337@gmail.com>
2016-01-23 13:23:30 -0500

    generic message test

--- public/app/js/modules/profile/show/profile.show.controller.js
+++ public/app/js/modules/profile/show/profile.show.controller.js
@@ -11,13 +11,12 @@
         var vm = this;
 
         console.log('---------------------------------');
-        console.log('---------- DEBUG ----------------');
+        console.log('---------- DEBUG - Profile Show -');
         console.log('---------------------------------');
 
         ProfileService
             .getById($stateParams.id)
             .then(function(response) {
-                console.log(JSON.stringify(response.data.data, null, '\t'));
                 vm.profile = response.data.data.profile;
                 vm.summoner = response.data.data.summoner;
 
@@ -26,7 +25,6 @@
                 vm.limited_languages = [];
 
                 _.forEach(vm.profile.languages, function(language, value) {
-                    console.log(language.fluency);
                     if(language.fluency == 'native')
                         vm.native_languages.push(language.type);
                     else if(language.fluency == 'professional')
@@ -35,9 +33,7 @@
                         vm.limited_languages.push(language.type);
                 });
 
-                console.log(JSON.stringify(vm.native_languages, null, '\t'));
-                console.log(JSON.stringify(vm.professional_languages, null, '\t'));
-
+                console.log(JSON.stringify(vm.summoner, null, '\t'));
             }, function() {
 
             });
